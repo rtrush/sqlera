9d898d4594ee15bde8ae6e2e3fdbc8b248d5c007
rtrush <86824811+rtrush@users.noreply.github.com>
2023-05-08 18:37:28 +0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Для создания новой таблицы в существующей базе данных используют команду",
+        answer: "CREATE TABLE",
+        options: [
+            "NEW TABLE",
+            "CREATE TABLE",
+            "MAKE TABLE",
+            "DO TABLE"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Как расшифровывается SQL?",
+        answer: "structured query language",
+        options: [
+            "structured query language",
+            "strict question line",
+            "strong question language",
+            "selected query language"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Для получения информации из базы данных служит команда",
+        answer: "SELECT",
+        options: [
+            "GET",
+            "READ",
+            "SELECT",
+            "VIEW"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Если выборка данных производится из нескольких таблиц, то это может указываться во фразе FROM следующим образом",
+        answer: "таблица1 INNER JOIN таблица2",
+        options: [
+            "таблица1 таблица2 ",
+            "таблица1 AND таблица2",
+            "таблица1 OUTER JOIN таблица2",
+            "таблица1 INNER JOIN таблица2"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Слово ALL во фразе SELECT означает",
+        answer: "не убирать строки-дубликаты из полученной выборки",
+        options: [
+            "все столбцы таблицы, указанной во фразе FROM",
+            "не убирать строки-дубликаты из полученной выборки",
+            "не делить выводимые данные на порции по размеру страницы ",
+            "все строки таблицы, указанной во фразе FROM "
+        ]
+    }
+]
